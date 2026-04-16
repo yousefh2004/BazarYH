26facefc79ac70fc3f23af7fb0f59cc6688f4c16
Yousef Yaser Hajeer <yousefhajeer384@gmail.com>
2026-04-16 22:21:07 +0300

    catalogLink

--- FrontendService/frontend.js
+++ FrontendService/frontend.js
@@ -1,23 +1,35 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
-app.get("/search/:topic", (req, res) => {
-  const topic = req.params.topic;
+const catalogURL = "http://localhost:5001";
 
-  res.json({
-    msg: "search route is working",
-    topic: topic
-  });
+app.get("/search/:topic", async (req, res) => {
+  const topic = req.params.topic;
+  try {
+    const response = await axios.get(`${catalogURL}/search/${topic}`);
+    res.json(response.data);
+  } catch (err) {
+      if (err.response) {
+      return res.status(err.response.status).json(err.response.data);
+    }
+    res.status(500).json({ msg: "catalog service error" });
+  }
+
 });
 
-app.get("/info/:id", (req, res) => {
+app.get("/info/:id", async (req, res) => {
   const id = req.params.id;
-
-  res.json({
-    msg: "info route is working",
-    id: id
-  });
+  try {
+    const response = await axios.get(`${catalogURL}/info/${id}`);
+    res.json(response.data);
+  } catch (err) {
+    if (err.response) {
+      return res.status(err.response.status).json(err.response.data);
+    }
+    res.status(500).json({ msg: "catalog service error" });
+  }
 });
 
 app.post("/purchase/:id", (req, res) => {
