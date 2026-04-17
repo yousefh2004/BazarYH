e7dcb9a01683297ee53543866d32e91530c23db4
Yousef Yaser Hajeer <yousefhajeer384@gmail.com>
2026-04-17 10:46:28 +0300

    orderLink

--- FrontendService/frontend.js
+++ FrontendService/frontend.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const app = express();
 
 const catalogURL = "http://localhost:5001";
+const orderURL = "http://localhost:5002";
 
 app.get("/search/:topic", async (req, res) => {
   const topic = req.params.topic;
@@ -32,13 +33,17 @@ app.get("/info/:id", async (req, res) => {
   }
 });
 
-app.post("/purchase/:id", (req, res) => {
+app.post("/purchase/:id", async (req, res) => {
   const id = req.params.id;
-
-  res.json({
-    msg: "purchase route is working",
-    id: id
-  });
+  try {
+    const response = await axios.post(`${orderURL}/purchase/${id}`);
+    res.json(response.data);
+  } catch (err) {
+    if (err.response) {
+      return res.status(err.response.status).json(err.response.data);
+    }
+    res.status(500).json({ msg: "order service error" });
+  }
 });
 
 app.listen(5000, () => {
